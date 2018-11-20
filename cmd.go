7b34b35d302274e69b139da84dc4419552539524
hel2o <601902498@qq.com>
2018-11-20 08:34:06 +0800

    add CmdUntil function

--- cmd.go
+++ cmd.go
@@ -10,3 +10,16 @@ func (c *TelnetClient) Cmd(cmd string) (string, error) {
 	return c.ReadUntil(c.prompt)
 }
 
+// Cmd sends command then wait special character and returns output
+func (c *TelnetClient) CmdUntil(cmd, until string) (out string, err error) {
+	err = c.Write([]byte(cmd))
+	if err != nil {
+		return
+	}
+
+	out, err = c.ReadUntil(until)
+	if err != nil {
+		return
+	}
+	return
+}
